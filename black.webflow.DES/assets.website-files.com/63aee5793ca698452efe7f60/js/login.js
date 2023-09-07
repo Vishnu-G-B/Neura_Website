9810cfeed99aa112b58e4b3436fd5d41b7733c32
Vishnu Bhattacharya <vishnubhattacharya.04@gmail.com>
2023-09-07 19:28:12 +0530

    most of the sign in / sign up logic is implemented

--- black.webflow.DES/assets.website-files.com/63aee5793ca698452efe7f60/js/login.js
+++ black.webflow.DES/assets.website-files.com/63aee5793ca698452efe7f60/js/login.js
@@ -1,29 +1,202 @@
 const endPoint = "https://neura-ai-backend.vercel.app/";
-function getDetails() {
+function signUpRequest() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const confPassword = document.getElementById("conf-password").value;
+    
+    if (password === confPassword) {
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", endPoint + "auth/signup");
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        const body = JSON.stringify({
+            email_id: username,
+            password: password,
+        });
+        xhr.onload = () => {
+            
+            let response = JSON.parse(xhr.responseText);
+            if(response.status === "200"){
+                createCookie("access_token", response.detail.user_state.access_token);
+                createCookie("refresh_token", response.detail.user_state.refresh_token);
+                createCookie("supabase_id", response.detail.user_state.supabase_id);
+            }
+            else {
+                console.log(response);
+                console.log("You probably have an account with us already");
+                appendAlert("ACCOUNT ALREADY EXISTS!");
+                setTimeout(() => {
+                
+                    try {
+                        const alertParentDiv = document.getElementById("liveAlertPlaceholder");
+                        const alertChild = document.getElementById("removable");
+                        alertParentDiv.classList.remove("liveAlertPlaceholder");
+                        alertChild.remove();
+                    } catch (error) {
+                       console.log("I can't be fkin asked"); 
+                    }
+                    
+                }, 3000);
+
+            }
+
+        };
+        xhr.send(body);
+
+        const xhr2 = new XMLHttpRequest();
+        xhr2.open("POST", endPoint + "event/register/1");
+        xhr2.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        const body2 = JSON.stringify ({
+            access_token: getCookie("access_token"),
+            refresh_token: getCookie("refresh_token"),
+            supabase_id: getCookie("supabase_id"),
+        });
+        xhr2.onload = () => {
+            let response = JSON.parse(xhr2.responseText);
+            if (response.status == 200) {
+                console.log (response);
+                createCookie("access_token", response.detail.user_state.access_token);
+                createCookie("refresh_token", response.detail.user_state.refresh_token);
+                createCookie("supabase_id", response.detail.user_state.supabase_id);
+                const qr_text = response.detail.jwt_qr;
+                window.location.replace("events.html");
+                createQRDiv(qr_text, "tempQRCode");
+            }
+            else {
+                console.log("ERROR",response);
+            }
+        };
+        xhr2.send(body2);
+
+
+    } else {
+        appendAlert("Passwords do not match");
+        setTimeout(() => {
+                
+            try {
+                const alertParentDiv = document.getElementById("liveAlertPlaceholder");
+                const alertChild = document.getElementById("removable");
+                alertParentDiv.classList.remove("liveAlertPlaceholder");
+                alertChild.remove();
+            } catch (error) {
+               console.log("I can't be fkin asked"); 
+            }
+            
+        }, 3000);
+    }
+}
+
+function signInRequest() {
+    const username = document.getElementById("username2").value;
+    const password = document.getElementById("password2").value;
+    const loginButton = document.getElementById("login");
+    loginButton.textContent = "Loading...";
+    loginButton.disabled = true;
+
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", endPoint + "auth/signup");
+    xhr.open("POST", endPoint + "auth/signin");
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     const body = JSON.stringify({
-        // access_token
-        //     :
-        //     "eyJhbGciOiJIUzI1NiIsImtpZCI6IjNXb01Ub1YrZ3IraW1aRlEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjkzODM5NDE3LCJpYXQiOjE2OTM4MzU4MTcsImlzcyI6Imh0dHBzOi8vamNuc2pxcWxnem5saHBlanN3bG0uc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjcyYWI0NDAzLWRjNDQtNGNjYS1iNjM2LWFkYjFmOGQ0MGM2MCIsImVtYWlsIjoiaGVsbG90aGVyZUBnbWFpbC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7fSwicm9sZSI6ImF1dGhlbnRpY2F0ZWQiLCJhYWwiOiJhYWwxIiwiYW1yIjpbeyJtZXRob2QiOiJwYXNzd29yZCIsInRpbWVzdGFtcCI6MTY5MzgzNTgxN31dLCJzZXNzaW9uX2lkIjoiMDA3ZGFhYWYtZWY4OS00MmU1LWE4OTItM2E3ODlkOWZmYzMxIn0.BG6K2fZP5AofJDxHwZzAZmzklRYiCwRC7ksgW9xAZIw",
-        // refresh_token
-        //     :
-        //     "m7iG2m-M_iwtfjqQ0fQLrA",
-        //     supabase_id
-        //     :
-        //     "72ab4403-dc44-4cca-b636-adb1f8d40c60",
         email_id: username,
         password: password,
     });
     xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 201) {
-            console.log(JSON.parse(xhr.responseText));
-        } else {
-            console.log(JSON.parse(xhr.responseText));
+        
+        let response = JSON.parse(xhr.responseText);
+        if (response.status === "200"){
+            createCookie("access_token", response.detail.user_state.access_token);
+            createCookie("refresh_token", response.detail.user_state.refresh_token);
+            createCookie("supabase_id", response.detail.user_state.supabase_id);
+            // console.log(document.cookie);
+            loginButton.textContent = "Logged In!"
+            window.location.replace("events.html");
+            
+        }
+        else {
+            console.log(response.status);
+            console.log("PROLLY wrong passwd or smthng");
+            appendAlert("WRONG PASSWORD DUMBFK");
+            setTimeout(() => {
+                
+                try {
+                    const alertParentDiv = document.getElementById("liveAlertPlaceholder");
+                    const alertChild = document.getElementById("removable");
+                    alertParentDiv.classList.remove("liveAlertPlaceholder");
+                    alertChild.remove();
+                } catch (error) {
+                   console.log("I can't be fkin asked",error.message); 
+                }
+                
+            }, 3000);
         }
     };
     xhr.send(body);
+}
+
+function getQR() {
+    const access_token_user = getCookie("access_token");
+    const refresh_token_user = getCookie("refresh_token");
+    const supabase_id_user = getCookie("supabase_id");
+    // console.log(typeof access_token_user,refresh_token_user,supabase_id_user)
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", endPoint + "event/fetch_qr");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    const body = JSON.stringify ({
+        access_token: access_token_user,
+        refresh_token: refresh_token_user,
+        supabase_id: supabase_id_user,
+    });
+
+    xhr.onload = () => {
+        let response = JSON.parse(xhr.responseText);
+        if (response.status == 200){
+            createCookie("access_token", response.detail.user_state.access_token);
+            createCookie("refresh_token", response.detail.user_state.refresh_token);
+            createCookie("supabase_id", response.detail.user_state.supabase_id);
+            const qr_text = response.detail.jwt_qr;
+            
+            createQRDiv(qr_text, "tempQRCode");
+        }
+    }
+    xhr.send(body);
+}
+
+// setTimeout(getQR,1000);
+
+function createCookie(key,value) { // Can add a 'expires' param here in the future
+    document.cookie = key + "=" + value + "; path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      c = c.trimStart();
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+    '<div class="alert" id="removable">',
+        `${message}`,
+    '</div>'
+    ].join('')
+    alertPlaceholder.classList.add("liveAlertPlaceholder");
+    alertPlaceholder.append(wrapper);
+}
+
+const createQRDiv = (content,divId) => {
+    var qrcode = new QRCode(document.getElementById(divId),
+    {
+        text: content,
+        correctLevel : QRCode.CorrectLevel.M
+    });
 }
